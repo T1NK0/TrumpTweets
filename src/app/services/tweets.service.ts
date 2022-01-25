22540df210a8779e152315429f45a0aff2c77839
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-01-25 19:49:24 +0100

    Printing data

--- src/app/services/tweets.service.ts
+++ src/app/services/tweets.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//Imports observable so we can make use of it in the HttpClient, that uses it for transactions.
+import { Observable } from 'rxjs';
+import { Tweets } from '../interfaces/tweet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrumpTweetsService {
 
-  constructor() { }
+  //
+  constructor(private http:HttpClient) {
+
+   }
+  //Sets the getTweets to observable,
+   getTweets (): Observable<Tweets[]> {
+     //returns the http
+    return this.http.get<Tweets[]>('./assets/data/trumpTweets.json');
+   }
 }
